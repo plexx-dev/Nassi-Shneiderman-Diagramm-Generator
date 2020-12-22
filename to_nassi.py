649e0ee9b53bfc6fefeec23b83b602a9baa659e8
weckyy702 <weckyy702@gmail.com>
2020-12-22 21:44:03 +0100

    Co-authored-by: max f. <simperlyofficial@gmail.com>

--- to_nassi.py
+++ to_nassi.py
@@ -2,7 +2,7 @@ from NassiShneidermann import NassiShneidermanDiagram
 from Iinstruction import *
 
 
-def nassi(filepath):
+def nassi(filepath:str ):
     NSD = NassiShneidermanDiagram(False)
     NSD.load_from_file(filepath)
-    NSD.convert_to_image("Nassi-Shneider-Diagramm", 500)
+    NSD.convert_to_image(filepath + "Nassi-Shneider-Diagramm", 500)
