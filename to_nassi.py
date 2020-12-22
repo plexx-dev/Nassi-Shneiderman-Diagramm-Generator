8284bd1d70b35ef093f567cbbb1b904287630ba5
oleting <info@ole-siepmann.de>
2020-12-22 20:49:59 +0100

    - debug

--- to_nassi.py
+++ to_nassi.py
@@ -3,6 +3,6 @@ from Iinstruction import *
 
 
 def nassi(filepath):
-    NSD = NassiShneidermanDiagram(True)
+    NSD = NassiShneidermanDiagram(False)
     NSD.load_from_file(filepath)
     NSD.convert_to_image("Nassi-Shneider-Diagramm", 500)
