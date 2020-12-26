cef0fd69c6884de92b55272c4488ab6578d36e15
oleting <info@ole-siepmann.de>
2020-12-26 14:53:01 +0100

    + test

--- gui/utils.py
+++ gui/utils.py
@@ -5,6 +5,7 @@ from draw.Iinstruction import *
 
 
 def nassi(filepath:str, output_path: str, gui):
+    print('')
     NSD = NassiShneidermanDiagram(gui=gui)
     NSD.load_from_file(filepath)
     NSD.convert_to_image(output_path + "/Nassi-Shneider-Diagramm", 500)
