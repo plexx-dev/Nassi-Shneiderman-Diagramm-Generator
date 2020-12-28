4aec8a2afd02541ae5f2861a84915637dbb863a5
oleting <info@ole-siepmann.de>
2020-12-28 19:40:19 +0100

    + fix gui

--- gui/gui.py
+++ gui/gui.py
@@ -101,22 +101,23 @@ class Gui:
 
         #new popup
 
-        text_column = [
-            [
-                sg.Text('What should the program do if a file already exists?')
-            ]
-        ]
-
-        choices = [
-            [
-                sg.Button(button_text='skip', key='-SKIP-'),
-                sg.Button(button_text='overwrite', key='-OVERWRITE-'),
-                sg.Button(button_text='create expicit name', key='-EXPICIT-'),
-            ]
-        ]
+       
 
         class layout_popup: 
             def __init__(self):
+                text_column = [
+                    [
+                        sg.Text('What should the program do if a file already exists?')
+                    ]
+                ]
+
+                choices = [
+                    [
+                        sg.Button(button_text='skip', key='-SKIP-'),
+                        sg.Button(button_text='overwrite', key='-OVERWRITE-'),
+                        sg.Button(button_text='create expicit name', key='-EXPICIT-'),
+                    ]
+                ]
                 self.layout = [
                         [
                             sg.Column(text_column),
